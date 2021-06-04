fcec6823ae154ad468b40fabcbd1e9951bb63ced
redfernj-OSU <81777875+redfernj-OSU@users.noreply.github.com>
2021-06-04 00:22:30 -0700

    fixed indent

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 import unittest
 import example
 
-    class TestCase(unittest.TestCase):
+class TestCase(unittest.TestCase):
 
-        def test_add_1(self):
-            self.assertEqual(example.add(1, 2), 3)
+    def test_add_1(self):
+        self.assertEqual(example.add(1, 2), 3)
 
 if __name__ == '__main__':
     unittest.main()
